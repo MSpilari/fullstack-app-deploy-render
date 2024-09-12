94a884d00c588eee4d6348e05255bb76aee2bb47
MSpilari <mb.spilari@hotmail.com>
2024-09-12 17:55:42 -0300

    feat: create delete message method

--- backend/src/main/java/com/spring/render/controllers/MessageController.java
+++ backend/src/main/java/com/spring/render/controllers/MessageController.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -36,4 +38,11 @@ public class MessageController {
         return ResponseEntity.status(HttpStatus.CREATED).body(newMessage);
     }
 
+    @DeleteMapping("/deletemessage/{id}")
+    public ResponseEntity<String> deleteMessage(@PathVariable("id") String id) {
+
+        messageService.deleteMessage(id);
+
+        return ResponseEntity.status(HttpStatus.NO_CONTENT).body(null);
+    }
 }
