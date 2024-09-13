b6ad8c0a06a7a8a8c8c4c42d4d34d1a1cd3acacd
MSpilari <mb.spilari@hotmail.com>
2024-09-13 16:33:24 -0300

    feat: add the button that erases a message

--- frontend/src/components/MessageList.tsx
+++ frontend/src/components/MessageList.tsx
@@ -1,12 +1,27 @@
 import { MessageDto } from "../dto/MessageDto";
 
-const MessageList = ({ messages }: { messages: MessageDto[] }) => {
+const MessageList = ({
+  messages,
+  eraseFunct,
+}: {
+  messages: MessageDto[];
+  eraseFunct: (id: String) => void;
+}) => {
   return (
     <section className="messageSection">
       <h1>Messages</h1>
       <ul>
         {messages.map((obj) => (
-          <li key={obj.id}>{obj.message}</li>
+          <li key={obj.id}>
+            {obj.message}{" "}
+            <button
+              onClick={() => {
+                eraseFunct(obj.id);
+              }}
+            >
+              Erase Message
+            </button>
+          </li>
         ))}
       </ul>
     </section>
